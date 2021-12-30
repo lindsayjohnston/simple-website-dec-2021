77a2101f7affe0ba872ee9788fdf3eb76f4bc2bf
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-12-30 13:44:03 -0800

    Removed all React boilerplate info. Added Bocaloud general info. Attempted to fix viewport height issue on mobile browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Adjust viewport height for mobile browser
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', ()=>{
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+let screenOrientation= null;
+if((window.innerHeight / window.innerWidth) < .87){
+  screenOrientation= "landscape";
+} else {
+  screenOrientation= "portrait";
+};
+
+class App extends React.Component {
+  
+  state= {
+
+      //state goes here
+  }
+
+  constructor(props){
+    super(props);
+    //all methods
+  }
+
+  render(){
+    //if stuff and functions
+
+    return (
+      <div className="App">
+        This will be the app.
+       
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
